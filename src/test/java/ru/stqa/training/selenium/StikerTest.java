fd35232e69a46fd35cb5f1fae07a18f7ddefc327
Сорокина Екатерина Игоревна <e.sorokina@ntc-vulkan.ru>
2017-03-21 16:53:37 +0300

    Задание 8

---
+++ src/test/java/ru/stqa/training/selenium/StikerTest.java
@@ -0,0 +1,52 @@
+package ru.stqa.training.selenium;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class StikerTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void beforeTest() {
+        System.out.println("Запуск браузера");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+
+    @After
+    public void afterTest() {
+        System.out.println("Завершение работы браузера");
+        driver.quit();
+    }
+
+    @Test
+    public void adminPanelTest() throws InterruptedException {
+        System.out.println("Открыть главную старницу ");
+        driver.get("http://localhost/litecart/en/");
+
+        System.out.println("Проверка наличия стикеров");
+        stickerCheck();
+    }
+
+    private void stickerCheck() {
+        List<WebElement> ducks = driver.findElements(By.className("product"));
+        System.out.println("Всего товаров : " + ducks.size());
+
+        for (WebElement duck : ducks) {
+            //количество стикеров у элемента
+            int stickersNum = duck.findElements(By.className("sticker")).size();
+            //System.out.println(stickersNum);
+            Assert.assertTrue(stickersNum == 1);
+        }
+
+    }
+}
+
