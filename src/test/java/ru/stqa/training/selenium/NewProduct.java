631153f2286940210cde4b0a4175710e199a5066
Сорокина Екатерина Игоревна <e.sorokina@ntc-vulkan.ru>
2017-03-23 19:50:58 +0300

    Задание 11

--- src/test/java/ru/stqa/training/selenium/NewProduct.java
+++ src/test/java/ru/stqa/training/selenium/NewProduct.java
@@ -16,5 +16,67 @@ package ru.stqa.training.selenium;
 Можно оформить сценарий либо как тест, либо как отдельный исполняемый файл.
  */
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.Random;
+import java.util.logging.Logger;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+
 public class NewProduct {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    private static Logger log = Logger.getLogger("MyLogger");
+
+    @Before
+    public void beforeTest() {
+        System.out.println("Запуск браузера");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @After
+    public void afterTest() {
+        System.out.println("Завершение работы браузера");
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    public void newProduct() throws InterruptedException {
+        System.out.println("Открыть старницу http://192.168.1.98/litecart/admin");
+        driver.get("http://localhost/litecart/admin");
+
+        // Вход
+        TestUtils.loginAsAdmin(driver, wait);
+
+        log.info("Добавление нового товара");
+        addProduct();
+
+        // Выход
+        TestUtils.logout(driver, wait);
+    }
+
+    public void addProduct() {
+
+        String firstname = TestUtils.generateString(new Random(), "qazwsxedcrfvtgbyhnujmikolp", 10);
+        driver.get("http://localhost/litecart/admin/?app=catalog&doc=catalog");
+        driver.findElement(By.linkText("Add New Product")).click();
+
+//        General
+        driver.findElement(By.cssSelector("[href='#tab-general']")).click();
+        wait.until(presenceOfElementLocated(By.cssSelector("[name=status]")));
+        driver.findElement(By.cssSelector("[name=status][value='1']")).click();
+
+
+
+    }
+
 }
--- src/test/java/ru/stqa/training/selenium/TrueProduct.java
+++ src/test/java/ru/stqa/training/selenium/TrueProduct.java
@@ -1,57 +1,185 @@
 package ru.stqa.training.selenium;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.util.*;
+import java.util.function.Supplier;
 
 /*
-а) на главной странице и на странице товара совпадает текст названия товара
-б) на главной странице и на странице товара совпадают цены (обычная и акционная)
-в) обычная цена зачёркнутая и серая (можно считать, что "серый" цвет это такой, у которого в RGBa представлении одинаковые значения для каналов R, G и B)
-г) акционная жирная и красная (можно считать, что "красный" цвет это такой, у которого в RGBa представлении каналы G и B имеют нулевые значения)
-(цвета надо проверить на каждой странице независимо, при этом цвета на разных страницах могут не совпадать)
-г) акционная цена крупнее, чем обычная (это тоже надо проверить на каждой странице независимо)
-*/
+ *   а) на главной странице и на странице товара совпадает текст названия товара
+ *   б) на главной странице и на странице товара совпадают цены (обычная и акционная)
+ *   в) обычная цена зачёркнутая и серая (можно считать, что "серый" цвет это такой, у которого в RGBa представлении одинаковые значения для каналов R, G и B)
+ *   г) акционная жирная и красная (можно считать, что "красный" цвет это такой, у которого в RGBa представлении каналы G и B имеют нулевые значения)
+ *   (цвета надо проверить на каждой странице независимо, при этом цвета на разных страницах могут не совпадать)
+ *   г) акционная цена крупнее, чем обычная (это тоже надо проверить на каждой странице независимо)
+ */
 
+@RunWith(Parameterized.class)
 public class TrueProduct {
+
+    private Supplier<WebDriver> webDriverSupplier;
     private WebDriver driver;
-    private WebDriverWait wait;
+    private DecimalFormat decimalFormat;
+
+    public TrueProduct(Supplier<WebDriver> webDriverSupplier) {
+        this.webDriverSupplier = webDriverSupplier;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> provideParameters() {
+        //noinspection RedundantArrayCreation
+        return Arrays.asList(new Object[][]{
+                {(Supplier<WebDriver>) ChromeDriver::new},
+                {(Supplier<WebDriver>) FirefoxDriver::new},
+                {(Supplier<WebDriver>) InternetExplorerDriver::new}
+        });
+    }
 
     @Before
     public void beforeTest() {
         System.out.println("Запуск браузера");
-        //System.setProperty("webdriver.chrome.driver", "E:\\Downloads\\chromedriver_win32\\chromedriver.exe");
-        driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, 10);
+//        System.setProperty("webdriver.chrome.driver", "E:\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        driver = webDriverSupplier.get();
+        decimalFormat = (DecimalFormat) DecimalFormat.getInstance();
+        decimalFormat.setParseBigDecimal(true);
     }
 
     @After
     public void afterTest() {
         System.out.println("Завершение работы браузера");
         driver.quit();
-        driver = null;
+        decimalFormat = null;
     }
 
     @Test
-    public void countryTest() throws InterruptedException {
+    public void productTest() throws InterruptedException {
         System.out.println("Открыть старницу http://192.168.1.98/litecart/admin");
-        driver.get("http://localhost/litecart/en");
-
-        System.out.println("Страны - проверка сортировки");
-        driver.findElement(By.cssSelector("#app-:nth-child(3)")).click();
-
-
-        System.out.println("Зоны - проверка сортировки, если есть в стране");
-
-
-        System.out.println("Геозоны - проверка сортировки списка зон");
-        driver.findElement(By.cssSelector("#app-:nth-child(6)")).click();
-
-
+        driver.get("http://192.168.1.98/litecart/en/");
+        List<WebElement> productContainers = getProductContainers(driver);
+        System.out.println("Found " + productContainers.size() + " products");
+        for (WebElement currentProductContainer : productContainers) {
+            ProductInfo productInfo = getProductInfo(currentProductContainer);
+            openProductDetails(currentProductContainer);
+            ProductInfo detailsProductInfo = getProductInfoFromDetailsScreen(driver);
+            assertSameProductInfo(productInfo, detailsProductInfo);
+            driver.switchTo().window(new ArrayList<>(driver.getWindowHandles()).get(0));
+        }
     }
 
+    private void assertSameProductInfo(ProductInfo productInfo, ProductInfo detailsProductInfo) {
+        System.out.println("ProductInfo: " + productInfo + ", detailsProductInfo: " + detailsProductInfo);
+        Assert.assertEquals(productInfo.getName(), detailsProductInfo.getName());
+        Assert.assertEquals(productInfo.getPrice(), detailsProductInfo.getPrice());
+        Assert.assertEquals(productInfo.getSpecialPrice(), detailsProductInfo.getSpecialPrice());
+    }
+
+    private void openProductDetails(WebElement productContainer) {
+        String selectLinkOpeninNewTab = Keys.chord(Keys.CONTROL, Keys.RETURN);
+        productContainer.findElement(By.className("link")).sendKeys(selectLinkOpeninNewTab);
+        List<String> windowHandles = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(windowHandles.get(windowHandles.size() - 1));
+    }
+
+    private List<WebElement> getProductContainers(WebDriver driver) {
+        return driver.findElements(By.className("product"));
+    }
+
+    private ProductInfo getProductInfo(WebElement productContainer) {
+        return ProductInfo.create(
+                getProductPrice(productContainer),
+                getProductSpecialPrice(productContainer),
+                getProductName(productContainer, By.className("name")));
+    }
+
+    private ProductInfo getProductInfoFromDetailsScreen(WebDriver driver) {
+        WebElement priceWrapper = driver.findElement(By.className("price-wrapper"));
+        return ProductInfo.create(
+                getProductPrice(priceWrapper),
+                getProductSpecialPrice(priceWrapper),
+                getProductName(driver.findElement(By.id("box-product")), By.className("title")));
+    }
+
+    private String getProductName(WebElement productContainer, By by) {
+        return productContainer.findElement(by).getText();
+    }
+
+    private BigDecimal getProductPrice(WebElement productContainer) {
+        Optional<WebElement> priceContainer = TestUtils.findElement(productContainer, By.className("price"));
+        if (!priceContainer.isPresent()) {
+            priceContainer = TestUtils.findElement(productContainer, By.className("regular-price"));
+        }
+        return parsePrice(priceContainer.get().getText());
+    }
+
+    private BigDecimal getProductSpecialPrice(WebElement productContainer) {
+        Optional<WebElement> priceContainer = TestUtils.findElement(productContainer, By.className("campaign-price"));
+        return priceContainer.isPresent()
+                ? parsePrice(priceContainer.get().getText())
+                : null;
+    }
+
+    private BigDecimal parsePrice(String priceText) {
+        if (priceText == null || priceText.isEmpty()) {
+            throw new IllegalArgumentException("Price text is empty");
+        }
+
+        try {
+            return (BigDecimal) decimalFormat.parse(priceText.replace("$", ""));
+        } catch (ParseException e) {
+            throw new IllegalStateException("Cannot parse price text");
+        }
+    }
+
+    private static class ProductInfo {
+
+        private final BigDecimal price;
+        private final String name;
+        private final BigDecimal specialPrice;
+
+        private ProductInfo(BigDecimal price, BigDecimal specialPrice, String name) {
+            this.price = price;
+            this.name = name;
+            this.specialPrice = specialPrice;
+        }
+
+        public static ProductInfo create(BigDecimal price, BigDecimal specialPrice, String name) {
+            return new ProductInfo(price, specialPrice, name);
+        }
+
+        public BigDecimal getPrice() {
+            return price;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Optional<BigDecimal> getSpecialPrice() {
+            return Optional.ofNullable(specialPrice);
+        }
+
+        @Override
+        public String toString() {
+            return "ProductInfo{" +
+                    "price=" + price +
+                    ", name='" + name + '\'' +
+                    ", specialPrice=" + specialPrice +
+                    '}';
+        }
+    }
 }
