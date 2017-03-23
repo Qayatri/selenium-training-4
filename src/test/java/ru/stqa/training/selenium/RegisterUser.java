c6470ee5c7b84fabca5347e9bb6e8c9e06407aca
Сорокина Екатерина Игоревна <e.sorokina@ntc-vulkan.ru>
2017-03-23 15:30:54 +0300

    Задание 11

--- src/test/java/ru/stqa/training/selenium/RegisterUser.java
+++ src/test/java/ru/stqa/training/selenium/RegisterUser.java
@@ -13,18 +13,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.Random;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
-import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
-/*
-Задание 11. Сделайте сценарий регистрации пользователя
-
-1) регистрация новой учётной записи с достаточно уникальным адресом электронной почты (чтобы не конфликтовало с ранее созданными пользователями, в том числе при предыдущих запусках того же самого сценария),
-2) выход (logout), потому что после успешной регистрации автоматически происходит вход,
-3) повторный вход в только что созданную учётную запись,
-4) и ещё раз выход.
-
-В форме регистрации есть капча, её нужно отключить в админке учебного приложения на вкладке Settings -> Security.
- */
 
 public class RegisterUser {
     private WebDriver driver;
@@ -96,7 +85,6 @@ public class RegisterUser {
         wait.until(elementToBeClickable(By.cssSelector("select[name=zone_code]")));
         Select selectState = new Select(driver.findElement(By.cssSelector("select[name=zone_code]")));
         selectState.selectByIndex(new Random().nextInt(selectState.getOptions().size()));
-
 
         if (driver.findElement(By.cssSelector("[name=newsletter]")).isSelected())
             driver.findElement(By.cssSelector("[name=newsletter]")).click();
